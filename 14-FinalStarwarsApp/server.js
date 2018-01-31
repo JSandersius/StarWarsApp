a9a57041bf0f506fbf034e8458bd03f5bc1ba692
Julian Sandersius <30510599+JSandersius@users.noreply.github.com>
2018-01-30 21:49:42 -0700

    secondary commit

--- 14-FinalStarwarsApp/server.js
+++ 14-FinalStarwarsApp/server.js
@@ -7,11 +7,13 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
 
 // Star Wars Characters (DATA)
 // =============================================================
